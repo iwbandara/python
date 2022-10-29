64bdfdb3a8ddee3ca25f078d51bdeb39fde27b26
Indika Bandara <40309775+iwbandara@users.noreply.github.com>
2022-10-29 22:34:23 +0530

    Add files via upload

---
+++ accept_input.py
@@ -0,0 +1,14 @@
+# program to accept age from a user and to display a message
+
+print("=================================")
+print()
+age=int(input("Please enter your age : "))
+print()
+print("=================================")
+
+if age >= 18:
+    print("You can Vote.")
+else:
+    print("You cannot Vote.")
+
+print("=================================")

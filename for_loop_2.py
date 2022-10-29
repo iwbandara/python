dd7200967e7540c5435eeeb53b20c4b2a114cd3a
Indika Bandara <40309775+iwbandara@users.noreply.github.com>
2022-10-29 23:01:26 +0530

    Add files via upload

---
+++ for_loop_2.py
@@ -0,0 +1,10 @@
+# Use of FOR loop to display values from 1 to 10
+
+for i in range (1,10):
+    print(i)
+
+print("--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+")
+# Use of FOR loop to display values from 1 to 10 with a step value of 2
+
+for i in range (1,10,2):
+    print(i)

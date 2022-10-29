13995e2a584a7a4daa3d3acc11c20d4c8a203de2
Indika Bandara <40309775+iwbandara@users.noreply.github.com>
2022-10-29 22:19:54 +0530

    Add files via upload

---
+++ print_command.py
@@ -0,0 +1,40 @@
+#printing some TEXT
+print("Hello World !!!")
+
+#printing some INTEGER
+print(100)
+
+#print both INTEGER & TEXT together
+print("I am 20 years old.")
+
+# Use of a variable for INTEGER
+val=20
+print(val)
+
+# Use of a variable for STRING
+str1="Hello"
+print(str1)
+
+# Converting an INTEGER value to a STRING
+print(str1 + str(val))
+
+# Converting a STRING to an INTEGER 
+#print(val+int(str1)) # this generates an ERROR
+
+# Convert a STRING into UPPERCASE
+print(str1.upper())
+
+# Convert a STRING into LOWERCASE
+upper_case=str1.upper()
+
+print(upper_case.lower())
+
+# Check the LENGTH of the STRING
+
+print(len(upper_case))
+
+# CONCATENATION of STRINGS
+name1="First String"
+name2="Second String"
+
+print(name1+" "+name2)

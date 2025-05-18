626e1fbdfd770eff1779e16c02241ec4e1746cda
Indika W Bandara <40309775+iwbandara@users.noreply.github.com>
2025-05-18 22:08:47 +0530

    Update for_loop_2.py

--- for_loop_2.py
+++ for_loop_2.py
@@ -8,3 +8,13 @@ print("--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+")
 
 for i in range (1,10,2):
     print(i)
+
+# Use the for loop to iterate the items in a List
+
+names = ['Saman', 'Kamal', 'Sameera']
+
+for name in names:
+    print(name) # displays on the name line by line
+
+for name in names:
+    print(name, end=' ') # display each name in one line separated with a space

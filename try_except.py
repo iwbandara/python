cf5edc7ae1136c716afdf05c9446b6e2778756e1
Indika W Bandara <40309775+iwbandara@users.noreply.github.com>
2025-05-21 22:30:42 +0530

    Update try_except.py

--- try_except.py
+++ try_except.py
@@ -7,3 +7,22 @@ while True: # Loops indefinitely until the user input is an integer
     break # Breaks out of the loop when the use enter an integer
 
 print(f'x is {x}')
+
+# ========================================================
+
+# Improve the cord with functions
+
+def main():
+  x = get_int()
+  print(f'x is {x}')
+  
+def get_int():
+  while True:
+    try:
+      x = int(input('What is x? '))
+    except ValueError:
+      print('x is not an integer')
+    else:
+      return x
+  
+main()        
